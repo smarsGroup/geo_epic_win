db358b7f5b2271c5fb6cb795fb59f87f7270a8bd
Bharath Irigireddy <iambharathchandra@gmail.com>
2025-05-21 15:13:10 -0700

    Update core.py
    Fix import

--- src/geoEpic/gee/core.py
+++ src/geoEpic/gee/core.py
@@ -3,7 +3,7 @@ import pandas as pd
 from ruamel.yaml import YAML
 from concurrent.futures import ThreadPoolExecutor
 from shapely.geometry import Polygon, MultiPolygon
-from geoEpic.utils.redis import WorkerPool
+from geoEpic.utils import WorkerPool
 
 import ee
 from geoEpic.gee.initialize import ee_Initialize
